16040603201d7c12a894a9e4dfa1c9c9bfbd7151
Gav1012 <gavin.poley@gmail.com>
2024-05-21 23:59:01 -0700

    added header file for jumping down site

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,13 @@
 export default function Header() {
-    console.log("hello from  header")
-}
+    console.log("hello from  header");
+    return (
+        <header>
+            <h1>Gavin Poley Portfolio</h1>
+            <nav>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/about";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
+import Header from "./components/header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
       <About />
       <Projects />
       <Contact />
