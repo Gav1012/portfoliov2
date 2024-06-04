276580eb1a095149d954b37cf5d83765d0d8932c
Gav1012 <gavin.poley@gmail.com>
2024-06-03 23:55:59 -0700

    updated header to change colors update to different sections

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,12 +2,13 @@ export default function Header() {
     console.log("hello from  header");
     return (
         <header>
-            <h1>Gavin Poley Portfolio</h1>
-            <nav>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
-            </nav>
+            <div className="flex justify-start items-center max-w-screen-xl">
+                <nav className="flex space-x-4">
+                    <a href="#about" className="hover:text-blue-400">About</a>
+                    <a href="#projects" className="hover:text-blue-400">Projects</a>
+                    <a href="#contact" className="hover:text-blue-400">Contact</a>
+                </nav>  
+            </div>
         </header>
     );
 };
