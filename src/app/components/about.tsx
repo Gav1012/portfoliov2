18722a32fe245979bd487e4ca4eb95f14e554d5b
Gav1012 <gavin.poley@gmail.com>
2024-06-03 23:59:16 -0700

    updated header font size, will update others

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -2,7 +2,7 @@ export default function About() {
     console.log("hello from about");
     return (
         <section id="about">
-            <h2>About Me</h2>
+            <h1 className="text-4x1">About Me</h1>
             <p>Example text about Gavin Poley</p>
         </section>
     );
