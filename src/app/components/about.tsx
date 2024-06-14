8938ce24650c35ee54b887ef20a3298185f191ac
Gav1012 <gavin.poley@gmail.com>
2024-06-13 23:59:46 -0700

    updated about and changed background color

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,9 +1,9 @@
 export default function About() {
     console.log("hello from about");
     return (
-        <section id="about">
-            <h1 className="text-6xl">About Me</h1>
-            <p className="text-lg max-w-2xl break-words">
+        <section id="about" className="py-16">
+            <h1 className="text-6xl mb-8">About Me</h1>
+            <p className="text-lg max-w-6xl px-4 break-words">
                 Hey I’m Gavin! I'm a graduate from the University of California Santa Cruz with a Bachelor’s in Computer Science. Most of my experience
                 as a developer has come from developing full-stack web applications on the frontend and the backend. I am currently 
                 volunteering for SpaceLab as a front end developer providing my experience as well as improving my skills with front-end development. 
