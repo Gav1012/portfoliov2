3852fc64a2483ffb838d891163739b28cd06104f
Gav1012 <gavin.poley@gmail.com>
2024-06-18 22:23:07 -0700

    updated header and footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
-    console.log("hello from footer");
     return(
-        <footer>
-            <hr></hr>
-            <p className="text-lg text-center py-4">Created by Gavin Poley</p>
+        <footer className="border-t border-gray-800 py-4">
+            <p className="text-xl text-center py-4">Created by Gavin Poley</p>
         </footer>
     );
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,14 +1,16 @@
 export default function Header() {
-    console.log("hello from  header");
     return (
-        <header>
-            <div className="flex justify-start items-center max-w-screen-xl">
-                <nav className="flex space-x-4">
-                    <a href="#about" className="hover:text-blue-400 text-2xl">About</a>
-                    <a href="#projects" className="hover:text-blue-400 text-2xl">Projects</a>
-                    <a href="#contact" className="hover:text-blue-400 text-2xl">Contact</a>
-                </nav>  
-            </div>
-        </header>
+        <nav id="header" className="border-b border-gray-800 py-3 px-3 sticky top-0 bg-[#121212] z-10">
+            <header>
+                <div className="flex justify-start items-center max-w-screen-xl">
+                    <nav className="flex space-x-4">
+                        <a href="#about" className="hover:text-blue-400 text-2xl">About</a>
+                        <a href="#experience" className="hover:text-blue-400 text-2xl">Experience</a>
+                        <a href="#projects" className="hover:text-blue-400 text-2xl">Projects</a>
+                        <a href="#contact" className="hover:text-blue-400 text-2xl">Contact</a>
+                    </nav>  
+                </div>
+            </header>
+        </nav>
     );
 };
