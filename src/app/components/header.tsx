a1905fde42df891938c8426a2120fbda219c64fb
Gav1012 <gavin.poley@gmail.com>
2024-06-19 23:31:54 -0700

    modified navbar and added smooth scrolling

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,16 @@
 export default function Header() {
     return (
-        <nav id="header" className="border-b border-gray-800 py-3 px-3 sticky top-0 bg-[#121212] z-10">
+        <section id="header" className="border-b border-gray-800 py-4 px-3 sticky top-0 bg-[#121212] z-10">
             <header>
-                <div className="flex justify-start items-center max-w-screen-xl">
+                <div className="flex justify-center items-center max-w-screen-l">
                     <nav className="flex space-x-4">
-                        <a href="#about" className="hover:text-blue-400 text-2xl">About</a>
-                        <a href="#experience" className="hover:text-blue-400 text-2xl">Experience</a>
-                        <a href="#projects" className="hover:text-blue-400 text-2xl">Projects</a>
-                        <a href="#contact" className="hover:text-blue-400 text-2xl">Contact</a>
+                        <a href="#about" className="text-2xl hover:text-blue-400 transition=all duration-300">About</a>
+                        <a href="#experience" className="text-2xl hover:text-blue-400 transition=all duration-300">Experience</a>
+                        <a href="#projects" className="text-2xl hover:text-blue-400 transition=all duration-300">Projects</a>
+                        <a href="#contact" className="text-2xl hover:text-blue-400 transition=all duration-300">Contact</a>
                     </nav>  
                 </div>
             </header>
-        </nav>
+        </section>
     );
 };
