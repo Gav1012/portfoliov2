e5e57d50adb0a4fc7b66b284a6e5ad3551ae52da
Gav1012 <gavin.poley@gmail.com>
2024-06-19 23:36:14 -0700

    minor adjustments to style and text

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,9 +1,9 @@
 export default function About() {
     return (
         <section id="about" className="py-16 px-10">
-            <h1 className="text-6xl mb-8">About Me</h1>
+            <h1 className="text-6xl mb-8">About Gavin</h1>
             <p className="text-lg max-w-6xl break-words">
-                Hey I’m Gavin! I'm a graduate from the University of California Santa Cruz with a Bachelor’s in Computer Science. Most of my experience
+                Hey, I’m Gavin! I'm a graduate from the University of California Santa Cruz with a Bachelor’s in Computer Science. Most of my experience
                 as a developer has come from developing full-stack web applications on the frontend and the backend. I am currently 
                 volunteering for SpaceLab as a front end developer providing my experience as well as improving my skills with front-end development. 
                 Currently I have been looking into learning more about topics relating to cloud development, AI, and machine learning.
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return(
         <footer className="border-t border-gray-800 py-4">
-            <p className="text-xl text-center py-4">Created by Gavin Poley</p>
+            <p className="text-xl text-center py-2">Created by Gavin Poley</p>
+            <p className="text-sm text-center">Built with: React, Next.js, TypeScript, TailwindCSS</p>
         </footer>
     );
 }
