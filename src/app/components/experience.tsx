1c1c9428cbe64668aa061ce662f4c3e27da2b32a
Gav1012 <gavin.poley@gmail.com>
2024-06-24 23:55:20 -0700

    added responsibilities to experience section

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -4,6 +4,11 @@ export default function Experience() {
             <h1 className="text-6xl mb-8">Experience</h1>
             <p className="text-lg max-w-6xl break-words">
                 Volunteer as Front End Developer @ Spacelab (will add link to website here)
+                ● Implemented a donation page with PayPal API integration, resulting in donations being made directly
+                    through the web page
+                ● Redesigned existing front end components in a teams of 2, enhancing the web page design, user
+                    interactivity, and user experience
+                ● Incorporated keyboard controls and alt tags for components, ensuring compliance with ADA standards
             </p>
         </section>
     )
