e379f9d8e5c18f612c02b932d9ea57438a0026aa
Gav1012 <gavin.poley@gmail.com>
2024-07-02 23:20:05 -0700

    slight changes to contact area

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -6,27 +6,27 @@ export default function Contact() {
             <p className="text-lg max-w-2xl break-words">
                 Feel free to reach out to me or view my resume!
             </p>
-            <div className="flex justify-start space-x-4 py-5">
+            <div className="flex justify-start space-x-5 py-5">
                 <a
                     href="https://www.linkedin.com/in/gavin-poley/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <FaLinkedin className="text-6xl border border-transparent rounded p-1 hover:border-blue-400 transition-all duration-300" />
+                    <FaLinkedin className="text-6xl border border-transparent rounded p-1 hover:border-blue-400 transition-all duration-300 hover:scale-105" />
                 </a>
                 <a
                     href="https://github.com/Gav1012"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <FaGithub className="text-6xl border border-transparent rounded p-1 hover:border-blue-400 transition-all duration-300" />
+                    <FaGithub className="text-6xl border border-transparent rounded p-1 hover:border-blue-400 transition-all duration-300 hover:scale-105" />
                 </a>
                 <a
                     href="/Gavin_Poley_Resume.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <FaFile className="text-6xl border border-transparent rounded p-1 hover:border-blue-400 transition-all duration-300" />
+                    <FaFile className="text-6xl border border-transparent rounded p-1 hover:border-blue-400 transition-all duration-300 hover:scale-105" />
                 </a>
             </div>
         </section>
