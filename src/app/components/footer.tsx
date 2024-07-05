b775f70837018e6df304351c9cbbaf28ac9d467b
Gav1012 <gavin.poley@gmail.com>
2024-07-04 20:28:42 -0700

    updated visuals and size

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
     return(
-        <footer className="border-t border-gray-800 py-4">
-            <p className="text-2xl text-center py-2">Created by Gavin Poley</p>
-            <p className="text-md text-center">Built with: React, Next.js, TypeScript, TailwindCSS</p>
+        <footer className="py-4 flex flex-col items-center">
+            <div className="w-3/4 border-t border-gray-100 py-4">
+                <p className="text-2xl text-center py-2">Created by Gavin Poley</p>
+                <p className="text-md text-center">Built with: React, Next.js, TypeScript, TailwindCSS</p>
+            </div>
         </footer>
     );
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,14 @@
 export default function Header() {
     return (
-        <section id="header" className="border-b border-gray-800 py-4 px-3 sticky top-0 bg-[#121212] z-10 shadow-white">
+        <section className="py-4 px-3 flex flex-col items-center sticky top-0 bg-[#121212] z-10 border-b border-gray-100">
             <header>
-                <div className="flex justify-center items-center max-w-screen-l">
-                    <nav className="flex space-x-4">
-                        <a href="#hero" className="text-3xl hover:text-blue-500 transition=all duration-300">Home</a>
-                        <a href="#about" className="text-3xl hover:text-blue-500 transition=all duration-300">About</a>
-                        <a href="#experience" className="text-3xl hover:text-blue-500 transition=all duration-300">Experience</a>
-                        <a href="#projects" className="text-3xl hover:text-blue-500 transition=all duration-300">Projects</a>
-                        <a href="#contact" className="text-3xl hover:text-blue-500 transition=all duration-300">Contact</a>
+                    <nav className="space-x-4">
+                        <a href="#hero" className="text-3xl transform hover:scale-105 hover:text-blue-500 transition-all duration-300">Home</a>
+                        <a href="#about" className="text-3xl hover:text-blue-500 transition-all duration-300">About</a>
+                        <a href="#experience" className="text-3xl hover:text-blue-500 transition-all duration-300">Experience</a>
+                        <a href="#projects" className="text-3xl hover:text-blue-500 transition-all duration-300">Projects</a>
+                        <a href="#contact" className="text-3xl hover:text-blue-500 transition-all duration-300">Contact</a>
                     </nav>  
-                </div>
             </header>
         </section>
     );
