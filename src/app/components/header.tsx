684c99d87155146059fda7f39b9730f43fb0f8c5
Gav1012 <gavin.poley@gmail.com>
2024-07-05 23:47:47 -0700

    added underline to header select and tracks current section

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,48 @@
+"use client"
+import { useState, useEffect } from 'react';
+
 export default function Header() {
+    const sections = ['hero', 'about', 'experience', 'projects', 'contact'];
+    const [activeSection, setActiveSection] = useState('');
+    
+    // handles the tracking of what section the user is currently at, adjusting the header to underline
+    // the current section of the website
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollPosition = window.scrollY + window.innerHeight / 2;
+    
+          sections.forEach((section) => {
+            const element = document.getElementById(section);
+            if (element) {
+              const offsetTop = element.offsetTop;
+              const offsetHeight = element.offsetHeight;
+    
+              if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
+                setActiveSection(section);
+              }
+            }
+          });
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
+
     return (
         <section className="py-4 px-3 flex flex-col items-center sticky top-0 bg-[#121212] z-10 border-b border-gray-100">
             <header>
                     <nav className="space-x-4">
-                        <a href="#hero" className="text-3xl transform hover:scale-105 hover:text-blue-500 transition-all duration-300">Home</a>
-                        <a href="#about" className="text-3xl hover:text-blue-500 transition-all duration-300">About</a>
-                        <a href="#experience" className="text-3xl hover:text-blue-500 transition-all duration-300">Experience</a>
-                        <a href="#projects" className="text-3xl hover:text-blue-500 transition-all duration-300">Projects</a>
-                        <a href="#contact" className="text-3xl hover:text-blue-500 transition-all duration-300">Contact</a>
+                    {sections.map((section) => (
+                        <a
+                            key={section}
+                            href={`#${section}`}
+                            className={`text-3xl hover:text-blue-500 transition-all duration-300 ${
+                                activeSection === section ? 'border-b-4 border-blue-500' : ''
+                            }`}
+                        >
+                        {section.charAt(0).toUpperCase() + section.slice(1)}
+                        </a>
+                    ))}
                     </nav>  
             </header>
         </section>
