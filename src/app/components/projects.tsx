7d1cf53733d8e973281280ef15d792e42e779ae3
Gav1012 <gavin.poley@gmail.com>
2024-07-31 23:55:29 -0700

    cleaned up file, made it 3 projects per row and changed css vals

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import React from 'react';
 
 export default function Projects() {
   // setting up redoing this part of the web, will manually update projects
@@ -23,26 +22,10 @@ export default function Projects() {
     }
   ];
 
-  // makes an API call to Github to grab projects
-  // might change this and manually update it so I can have
-  // more personalization
-  // useEffect(() => {
-  //   const fetchGitHubProjects = async () => {
-  //       const username = 'Gav1012';
-  //       const url = `https://api.github.com/users/${username}/repos`;
-  //       const response = await fetch(url);
-  //       const repos = await response.json();
-  //       const selectedProjects = ['flixforum', 'Facebook-MarketPlace-Clone', 'Three.js-Assignment'];
-  //       const filteredRepos = repos.filter((repo) => selectedProjects.includes(repo.name));
-  //       setProjects(filteredRepos);
-  //   };
-  //   fetchGitHubProjects();
-  // }, []);
-
   return (
-    <section id="projects" className="py-16 px-10 scroll-mt-20">
+    <section id="projects" className="mb-32 mt-10 px-10 scroll-mt-28">
       <h1 className="text-7xl md:text-9xl mb-8 text-center">Projects</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-12">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-12">
         {projects.map((project) => (
           <div key={project.id} className="bg-blue-900 p-6 rounded-xl border border-transparent rounded hover:border-blue-100 transition-all duration-300 transform hover:scale-105">
             <h2 className="text-center text-3xl font-bold mb-2">{project.name}</h2>
